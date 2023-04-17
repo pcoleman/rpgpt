22bd78026e5cc5ff388b83a5905e4d7343535e31
pcoleman <pbcoleman@gmail.com>
2023-04-17 01:11:10 -0500

    Update rpgpt.js

--- rpgpt.js
+++ rpgpt.js
@@ -246,7 +246,7 @@ function nextPlayerCreationStage() {
     // Create new message to send to chatGPT
     var message = [{"role":"user", "content": "Provide a detailed description of this next step of character creation: " + stageText + ", using the " + mechanics + " in the " + setting + " setting. Provide a description of the options. Here are the choices I've made so far: " + characterString + "Format everything using html code."}];
     
-    console.log(JSON.stringify(message);
+    console.log(JSON.stringify(message));
     // Send prompt
     prompt(message, function(msg) {
       var nextStep = msg.choices[0].message.content;
