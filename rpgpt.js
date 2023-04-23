91e36578bcad0480aa4c52688f1181bf70285cb8
pcoleman <pbcoleman@gmail.com>
2023-04-22 22:57:26 -0500

    Update rpgpt.js

--- rpgpt.js
+++ rpgpt.js
@@ -80,7 +80,7 @@ function startAdventure(event) {
 function submitAction(event) {
 	var sessionName = localStorage.getItem("currentSession");
 	var playerName = localStorage.getItem(sessionName + ".current-player");
-	var action = $("text-submit-area").val();
+	var action = document.querySelector('#text-submit-area').value;
 	
 	console.log(action);
 	
